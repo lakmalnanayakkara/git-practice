6aecd418b6137398caebd181f9dfb5d88428103b
lakmalpasindu <nanayakk-cs18027@stu.kln.ac.lk>
2025-06-02 03:03:16 +0530

    feat:json config

--- server.js
+++ server.js
@@ -15,6 +15,8 @@ mongoose
 
 const app = express();
 
+app.use(express.json());
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server at http://localhost:${port}`);
